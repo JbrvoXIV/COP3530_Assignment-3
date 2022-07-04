cf0115a5c795a4488d357a0242790e6bb70decb7
JbrvoXIV <jbravo0423@gmail.com>
2022-07-04 00:22:36 -0400

    added sumOfUniqueElements method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Main {
 
-    public static char findDistinguishingChar(String str1, String str2) {
+    public static char findDistinguishingChar(String str1, String str2) { // PROG PART 1
 
         HashMap<Character, Integer> str1Map = new HashMap<>();
         HashMap<Character, Integer> str2Map = new HashMap<>();
@@ -52,7 +53,34 @@ public class Main {
         return maxDiffLetter;
     }
 
+    public static int sumOfUniqueElements(int[] arr) { // PROG PART 3
+
+        HashMap<Integer, Integer> valueUniqueness = new HashMap<>();
+        int sumOfUniqueVals = 0;
+
+        for(int val : arr) { // ADD ELEMENTS FROM ARRAY TO HASHMAP AND INCREASE COUNTER BASED ON FREQUENCY
+            if(!valueUniqueness.containsKey(val))
+                valueUniqueness.put(val, 0);
+            valueUniqueness.put(val, valueUniqueness.get(val) + 1);
+        }
+
+        for(int val : arr) { // LOOP THROUGH HASHMAP, INCREASING SUM WITH UNIQUE VALUE FOUND
+            if(valueUniqueness.get(val) == 1)
+                sumOfUniqueVals += val;
+        }
+
+        return sumOfUniqueVals;
+    }
+
     public static void main(String[] args) {
         System.out.println("Character with highest difference of appearance in both strings: " + findDistinguishingChar("apple", "pale"));
+
+        Random rnd = new Random();
+        int[] arr = new int[20];
+        for(int i = 0; i < 20; i++) {
+            arr[i] = rnd.nextInt(10);
+            System.out.printf("%d,", arr[i]);
+        }
+        System.out.println("\n" + sumOfUniqueElements(arr));
     }
 }
